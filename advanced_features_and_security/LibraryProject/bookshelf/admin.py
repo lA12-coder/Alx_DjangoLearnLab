e04dc5ed3718c8351593baf6fc60d7190dd5676c
Lidet <lidetadmassu217@gmail.com>
2025-09-14 23:45:52 +0300

    advanced security features

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Book
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as UserBaseAdmin
+from .models import User
+# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+
+
+@admin.register(User)
+class CustomUserAdmin(UserBaseAdmin):
+    model=User
+    list_display = ('username','email', 'first_name', 'last_name','is_staff', 'date_of_birth')
+    fieldsets = UserBaseAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserBaseAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -4,40 +4,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class CustomUserManager(BaseUserManager):
-    use_in_migrations=True
-
-    #create a new user
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    #create a superuser
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_staff", True)
-
-        if not extra_fields.get("is_staff"):
-            raise ValueError("Superuser must have is_staff=True")
-        if not extra_fields.get('is_superuser'):
-            raise ValueError("Superuser must have is_superuser=True")
-        return self.create_user(email, password, **extra_fields)
-
-
-class User(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField("Profile Picture", upload_to="accounts/profile_photo/", blank=True, null=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
 
 
 class Author(models.Model):
