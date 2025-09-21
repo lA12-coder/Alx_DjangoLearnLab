0ba514637301806b473d76b25f902ee12589887b
Lidet <lidetadmassu217@gmail.com>
2025-09-21 21:52:29 +0300

    permission views

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,29 @@
+# your_app_label/forms.py
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter book title'
+            }),
+            'author': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter author name'
+            }),
+            'publication_year': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter publication year'
+            }),
+        }
+
+        labels = {
+            'title': 'Book Title',
+            'author': 'Author',
+            'publication_year': 'Publication Year',
+        }
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,1 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from .forms import BookForm
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Book created")
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'your_app_label/book_form.html', {'form': form})
+
+@login_required
+@permission_required('your_app_label.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Book updated.")
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'your_app_label/book_form.html', {'form': form, 'book': book})
+  
