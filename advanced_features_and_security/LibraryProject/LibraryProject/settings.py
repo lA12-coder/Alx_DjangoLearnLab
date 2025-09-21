d44a8ecbcd7b81d989552f051800425d1d18692b
Lidet <lidetadmassu217@gmail.com>
2025-09-21 23:44:18 +0300

    API

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -92,7 +92,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf.apps.BookshelfConfig'
+    'bookshelf.apps.BookshelfConfig',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
