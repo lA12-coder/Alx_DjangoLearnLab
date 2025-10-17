cad0253c282318770d7b0e32c442337f2f3b1884
Lidet <lidetadmassu217@gmail.com>
2025-10-17 17:48:50 +0300

    Fix a bug

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import  path
 from .views import *
+from ..posts.views import FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name='login-view'),
     path("register/", RegistrationView.as_view(), name='register'),
     path("profile/", UserProfileView.as_view(), name='Profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
