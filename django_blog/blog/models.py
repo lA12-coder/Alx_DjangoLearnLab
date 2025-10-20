9b76ff0d00076877d36fe398db88ac5b678579c4
Lidet <lidetadmassu217@gmail.com>
2025-10-20 15:11:13 +0300

    feat: Post model, and setting configuration for templeates and static files

---
+++ django_blog/blog/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
