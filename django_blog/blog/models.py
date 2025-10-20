de95fe9c867f7a319b997435c566aa92bd85d8d5
Lidet <lidetadmassu217@gmail.com>
2025-10-20 15:13:08 +0300

    Bug fix on the user model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,5 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-User = get_user_model()
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
